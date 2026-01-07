10dea699e709d5c0300a7b2786e035b8626bf991
stamp <nantiya02101999@gmail.com>
2026-01-07 17:38:15 +0700

    Nearly finished pet card feature.

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ async function petsArea() {
     const clone = template.content.cloneNode(true)
 
     clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
 
     wrapper.appendChild(clone)
   })
@@ -26,3 +30,14 @@ async function petsArea() {
 }
 
 petsArea()
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear()
+  const birthYearNumber = Number(birthYear)
+  const age = currentYear - birthYearNumber
+
+  if (age <= 0) return "Less than a year old"
+  if (age === 1) return "1 year old"
+
+  return `${age} years old`
+}
